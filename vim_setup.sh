eeea82b1ba7ba0c8693a48abd317d02055334030
haoran <haoran.yi@gmail.com>
2022-10-18 19:14:07 +0000

    up

---
+++ vim_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -fLo ~/.vim/colors/zenburn.vim --create-dirs https://raw.githubusercontent.com/jnurmine/Zenburn/master/colors/zenburn.vim
+curl -fLo ~/.vim/colors/gruvbox.vim --create-dirs https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
+curl -fLo ~/.vim/autoload/gruvbox.vim --create-dirs https://raw.githubusercontent.com/morhetz/gruvbox/master/autoload/gruvbox.vim
+
